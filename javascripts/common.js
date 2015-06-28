4d448858f2797d1d45adf32a9c424fd6faffe56e
elishayer <eshayer@stanford.edu>
2015-06-28 14:15:05 -0800

    Built out common elements

--- javascripts/common.js
+++ javascripts/common.js
@@ -1,7 +1,8 @@
-(function(window, document, undefined) {
+populatePage = function(pageTemplate, pageData, divId) {
 
 	// Create handlebars template
 	var templates = {};
+	templates.pageTemplate = Handlebars.compile(pageTemplate);
 	templates.banner = Handlebars.compile(Templates.banner);
 	templates.title = Handlebars.compile(Templates.title);
 	templates.callToAction = Handlebars.compile(Templates.callToAction);
@@ -13,9 +14,24 @@
 	links.facebook = { name: "Facebook", link: "www.facebook.com/stanfordsportsanalytics" };
 	links.blog = { name: "Blog", link: "www.stanfordsportsanalytics.wordpress.com" };
 
-	$('#banner').html(templates.banner());
-	$('#title').html(templates.title());
-	$('#call-to-action').html(templates.callToAction());
-	$('#footer').html(templates.footer());
+	var buttons = {};
+	buttons.banner = [
+		{text: links.home.name, link: links.home.link, color: "dark"},
+		{text: "Profiles", link: "www.stanfordsportsanalytics.com/profiles", color: "dark"},
+		{text: "Blog", link: "http://stanfordsportsanalytics.wordpress.com", color: "dark", target: "new"}
+	];
+	buttons.mailList = {text: "Join our mailing list!", link: "//mailman.stanford.edu/mailman/listinfo/sportsanalytics", color: "light", target: "new"};
 
-})(this, this.document);
+	var data = {};
+	data.links = links;
+	data.buttons = buttons;
+	data.year = new Date().getFullYear();
+	data.pageData = pageData
+
+	$("#banner").html(templates.banner(data));
+	$("#title").html(templates.title(data));
+	$("#call-to-action").html(templates.callToAction(data));
+	$("#footer").html(templates.footer(data));
+	$(divId).html(templates.pageTemplate(data));
+
+};
