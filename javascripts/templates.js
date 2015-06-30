f1bb4a01befc700bbfe0ce3faacf207a889329c6
elishayer <eshayer@stanford.edu>
2015-06-29 21:36:45 -0800

    Remove member template to page js file

--- javascripts/templates.js
+++ javascripts/templates.js
@@ -1,38 +1,5 @@
 var Templates = {};
 
-Templates.members = [
-'{{#with pageData}}',
-    '{{#if members}}',
-        '{{#each members}}',
-            '<div class="row">',
-                '<div class="col-xs-12 col-sm-6 col-md-3">',
-                    '<h3><b>{{ name }}</b></h3>',
-                    '{{#if position}} <h4>{{ position }}</h4> {{/if}}',
-                    '{{#if joined}} <h4>Member since {{ joined }}</h4> {{/if}}',
-                '</div>',
-                '<div class="col-xs-12 col-sm-6 col-md-3">',
-                    '{{#if image}} <img alt={{name}} src="images/{{image}}" height="120px"> {{/if}}',
-                '</div>',
-                '<div class="col-xs-12 col-md-6">',
-                    '<p>{{ description }}</h4>',
-                    '{{#if posts}}',
-                        '<h4>Blog posts:</h4>',
-                        '<ul style="list-style-type:disc">',
-                            '{{#each posts}}',
-                                '<li><a href="{{link}}">{{title}}</a></li>',
-                            '{{/each}}',
-                        '</ul>',
-                    '{{/if}}',
-                '</div>',
-            '</div>',
-            '<hr>',
-        '{{/each}}',
-    '{{else}}',
-        '<span>No members found.</span>',
-    '{{/if}}',
-'{{/with}}'
-].join('\n');
-
 Templates.banner = [
 '<div class="col-xs-12 col-sm-3 col-md-3">',
     '<a href="http://stanfordsportsanalytics.com/"><img alt="Stanford Sports Analytics Club" src="https://lh3.googleusercontent.com/LzcHfBQXmDdcmpFcp59uA1SDpwOxPpIJmbKqcJO2L0Q=w182-h103-no"></a>',
