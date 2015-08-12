46788db635ad690e3e66cb69627515957112e831
elishayer <eshayer@stanford.edu>
2015-08-12 09:12:59 -0800

    Add targets to anchor tags

--- javascripts/templates.js
+++ javascripts/templates.js
@@ -2,11 +2,11 @@ var Templates = {};
 
 Templates.banner = [
 '<div class="col-xs-12 col-sm-3 col-md-3">',
-    '<a href="http://stanfordsportsanalytics.com/"><img alt="Stanford Sports Analytics Club" src="https://lh3.googleusercontent.com/LzcHfBQXmDdcmpFcp59uA1SDpwOxPpIJmbKqcJO2L0Q=w182-h103-no"></a>',
+    '<a href="http://stanfordsportsanalytics.com/" target="_parent"><img alt="Stanford Sports Analytics Club" src="https://lh3.googleusercontent.com/LzcHfBQXmDdcmpFcp59uA1SDpwOxPpIJmbKqcJO2L0Q=w182-h103-no"></a>',
 '</div>',
 '<div class="col-xs-12 col-sm-8 col-md-9">',
     '{{#each buttons.banner}}',
-    '<a href="{{ link }}" class="btn btn-lg btn-{{ color }}" {{#if target}}target="new"{{/if}}>{{ text }}</a>',
+    '<a href="{{ link }}" class="btn btn-lg btn-{{ color }}" target="{{#if target}}new{{else}}_parent{{/if}}">{{ text }}</a>',
     '{{/each}}',
 '</div>'
 ].join('\n');
@@ -24,7 +24,7 @@ Templates.callToAction = [
         '<div class="col-xs-12 text-center">',
             '<h3>Get Involved</h3>',
             '{{#with buttons.mailList}}',
-            '<a href="{{ link }}" class="btn btn-lg btn-{{ color }}" {{#if target}}target="new"{{/if}}>{{ text }}</a>',
+            '<a href="{{ link }}" class="btn btn-lg btn-{{ color }}" target="{{#if target}}new{{else}}_parent{{/if}}">{{ text }}</a>',
             '{{/with}}',
         '</div>',
     '</div>',
@@ -36,8 +36,8 @@ Templates.footer = [
     '<div class="row">',
         '<div class="col-xs-12 text-center">',
             '<ul class="list-inline contact">',
-                '<li><a href="{{ links.facebook.link }}"><i class="fa fa-facebook fa-fw fa-3x"></i></a></li>',
-                '<li><a href="{{ links.twitter.link }}"><i class="fa fa-twitter fa-fw fa-3x"></i></a></li>',
+                '<li><a href="{{ links.facebook.link }}" target="_parent"><i class="fa fa-facebook fa-fw fa-3x"></i></a></li>',
+                '<li><a href="{{ links.twitter.link }}" target="_parent"><i class="fa fa-twitter fa-fw fa-3x"></i></a></li>',
             '</ul>',
             '<hr class="small">',
             '<p class="text-muted">Copyright &copy; Stanford Sports Analytics Club {{ year }}</p>',
@@ -54,9 +54,9 @@ Templates.sidebar = [
 '<div class="sidebar-module">',
     '<h4>Elsewhere</h4>',
     '<ol class="list-unstyled">',
-        '<li><a href="{{ links.twitter.link}}">{{ links.twitter.name}}</a></li>',
-        '<li><a href="{{ links.facebook.link }}">{{ links.facebook.name }}</a></li>',
-        '<li><a href="{{ links.blog.link }}">{{ links.blog.name }}</a></li>',
+        '<li><a href="{{ links.twitter.link }}" target="_parent">{{ links.twitter.name}}</a></li>',
+        '<li><a href="{{ links.facebook.link }}" target="_parent">{{ links.facebook.name }}</a></li>',
+        '<li><a href="{{ links.blog.link }}" target="_parent">{{ links.blog.name }}</a></li>',
     '</ol>',
 '</div>',
 ].join('\n');
