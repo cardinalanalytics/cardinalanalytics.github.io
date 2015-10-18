a03659fa85432e71d868fbb9551bb340113abd46
elishayer <eshayer@stanford.edu>
2015-10-17 19:40:23 -0700

    Jobs active/closed sections

--- js/jobs.js
+++ js/jobs.js
@@ -2,94 +2,121 @@
 
 	// data specifically for the jobs page
 	var jobsData = {
-		header: "Jobs Postings",
+		header: "Job Postings",
 		description: "Opportunities to work in sports analytics",
-		navbarTitle: "Jobs",
-		jobs: [
-			{
-				company: "Charlotte Hornets",
-				title: "Business Analytics Internship",
-				location: "Charlotte, NC",
-				description: "The Business Analytics and Database Marketing Intern will be interning in our offices here in Charlotte, NC. The primary responsibility of this intern will be to assist the Business Intelligence department’s needs with business analytics.",
-				link: "http://nbateamjobs.teamworkonline.com/teamwork/r.cfm?i=87460",
-				logo: "hornets.png",
-				league: "nhl.png",
-				date: {
-					posted: {
-						year: 2015,
-						month: 10,
-						day: 5
+		navbarTitle: "Job Postings",
+		overview: [
+			"These job listings are maintained for your convenience.",
+			"If you have found a job listing that would fit on this page, please submit the link here: ",
+			'<input type="text" id="jobLink" placeholder="Job Link">'
+		],
+		jobs: {
+			active: [
+				{
+					company: "Charlotte Hornets",
+					title: "Business Analytics Internship",
+					location: "Charlotte, NC",
+					description: "The Business Analytics and Database Marketing Intern will be interning in our offices here in Charlotte, NC. The primary responsibility of this intern will be to assist the Business Intelligence department’s needs with business analytics.",
+					link: "http://nbateamjobs.teamworkonline.com/teamwork/r.cfm?i=87460",
+					logo: "hornets.gif",
+					date: {
+						posted: {
+							year: 2015,
+							month: 10,
+							day: 5
+						}
+					}
+				},
+				{
+					company: "San Francisco 49ers",
+					title: "Data Science Internship",
+					location: "San Francisco, CA",
+					description: "We are looking for a Data Science Intern to play a critical role in our use and understanding of data across various 49ers technology efforts and fan interaction and touch points.",
+					link: "http://49ers.teamworkonline.com/teamwork/r.cfm?i=83653",
+					logo: "49ers.gif",
+					date: {
+						posted: {
+							year: 2015,
+							month: 9,
+						}
+					}
+				},
+				{
+					company: "Columbus Crew SC",
+					title: "Database & Analytics Internship",
+					location: "Columbus, OH",
+					description: "Columbus Crew SC is offering a Database and Analytics Internship designed to offer experience in reviewing and analyzing data within our Sales & Marketing Department.",
+					link: "http://mls.teamworkonline.com/teamwork/r.cfm?i=87042",
+					logo: "columbus-crew.gif",
+					date: {
+						posted: {
+							year: 2015,
+							month: 9,
+							day: 28
+						}
 					}
 				}
-			},
-			{
-				company: "San Francisco 49ers",
-				title: "Data Science Internship",
-				location: "San Francisco, CA",
-				description: "We are looking for a Data Science Intern to play a critical role in our use and understanding of data across various 49ers technology efforts and fan interaction and touch points.",
-				link: "http://49ers.teamworkonline.com/teamwork/r.cfm?i=83653",
-				logo: "49ers.png",
-				league: "nfl.png",
-				date: {
-					posted: {
-						year: 2015,
-						month: 9,
+			],
+			closed: [
+				{
+					company: "Carolina Hurricanes",
+					title: "Java Developer/Database Administrator",
+					location: "Raleigh, NC",
+					description: "The Carolina Hurricanes and PNC Arena are seeking a Java Developer to continue development of existing systems and write new applications as needed.",
+					link: "http://hockeyjobs.nhl.com/teamwork/r.cfm?i=85191",
+					logo: "hurricanes.gif",
+					date: {
+						posted: {
+							year: 2015,
+							month: 9,
+						}
 					}
 				}
-			},
-			{
-				company: "Colmubus Crew SC",
-				title: "Database & Analytics Internship",
-				location: "Columbus, OH",
-				description: "Columbus Crew SC is offering a Database and Analytics Internship designed to offer experience in reviewing and analyzing data within our Sales & Marketing Department.",
-				link: "http://mls.teamworkonline.com/teamwork/r.cfm?i=87042",
-				logo: "columbus-crew.png",
-				league: "mls.png",
-				date: {
-					posted: {
-						year: 2015,
-						month: 9,
-						day: 28
-					}
-				}
-			},
-			{
-				company: "Carolina Hurricanes",
-				title: "Java Developer/Database Administrator",
-				location: "Raleigh, NC",
-				description: "The Carolina Hurricanes and PNC Arena are seeking a Java Developer to continue development of existing systems and write new applications as needed.",
-				link: "http://hockeyjobs.nhl.com/teamwork/r.cfm?i=85191",
-				logo: "hurricanes.png",
-				league: "nhl.png",
-				date: {
-					posted: {
-						year: 2015,
-						month: 9,
-					}
-				}
-			}
-		]
+			]
+		}
 	};
+
+	Handlebars.registerHelper("job", function(job, closed) {
+		var result = "";
+		result += '<div class="row job-posting">'; // JOB POSTING
+
+		// add job logo
+		result += '<div class="col-sm-2 col-xs-4">';
+		result += '<a href="' + job.link + '" target="_new"><img src="./images/jobs/' + job.logo + '" class="job-image"/></a>';
+		result += '</div>';
+
+		// add company name, location, title, and date posted
+		result += '<div class="col-sm-3 col-xs-8">';
+		result += '<h3>' + job.company + '</h3>';
+		result += '<h4>' + job.location + '</h4>';
+		result += '<h4>' + job.title + '</h4>';
+		result += '<h5>' + Handlebars.helpers.textualDate(job.date.posted) + '</h5>';
+		result += '</div>';
+
+		// add the description and link
+		result += '<div class="col-sm-7 col-xs-12">';
+		result += '<p>' + job.description + '</p>';
+		result += '<a href="' + job.link + '" target="_new">Link to the ' + job.title + ' job with the ' + job.company + '</a>';
+		result += '</div>';
+
+		result += '</div>'; // / JOB POSTING
+		console.log();
+		return new Handlebars.SafeString(result);
+	});
 	
 	// Handlebars template for the jobs page
 	jobsTemplate = [
+	//	'{{#each pageData.overview}}',
+	//		'<p>{{{ this }}}</p>',
+	//	'{{/each}}',
 		'<div class="row">',
-			'{{#each pageData.jobs}}',
-				'<div class="row job-posting">',
-					'<div class="col-sm-2 col-xs-4">',
-						'{{#if logo}}<a href="{{ link }}" target="_new"><img src="./images/jobs/{{ logo }}" class="job-image"/></a>{{/if}}',
-					'</div>',
-					'<div class="col-sm-3 col-xs-8">',
-						'<h3>{{ company }}</h3>',
-						'<h4>{{ location }}</h4>',
-						'<h4>{{ title }}</h4>',
-						'<h5>Posted {{textualDate date.posted}}</h5>',
-					'</div>',
-					'<div class="col-sm-7 col-xs-12">',
-						'<p>{{ description }}</p>',
-						'<a href="{{ link }}" target="_new">Link to the {{ title }} job with the {{ company }}</a>',
-					'</div>',
-				'</div>',
+			'<h2 class="center">Active Jobs</h2>',
+			'{{#each pageData.jobs.active}}',
+				'{{job this}}',
+			'{{/each}}',
+			'<h2 class="center">Closed Jobs</h2>',
+			'{{#each pageData.jobs.closed}}',
+				'{{job this}}',
 			'{{/each}}',
 		'</div>'
 	].join('\n');
