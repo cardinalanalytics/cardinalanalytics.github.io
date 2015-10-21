0f4641fca4d6a5431631c99050173eb3082a46fc
elishayer <eshayer@stanford.edu>
2015-10-20 21:57:18 -0700

    Active/closed jobs using Handlebars Helpers

--- js/jobs.js
+++ js/jobs.js
@@ -10,174 +10,170 @@
 			"If you have found a job listing that would fit on this page, please submit the link here: ",
 			'<input type="text" id="jobLink" placeholder="Job Link">'
 		],
-		jobs: {
-			active: [
-				{
-					company: "Los Angeles Angels of Anaheim",
-					title: "Director, Quantitative Analysis",
-					location: "Anaheim, CA",
-					description: "Responsible for developing and overseeing the research and analytics initiative within Baseball Operations, reporting to the General Manager and Assistant General Manager.",
-					link: "http://baseballjobs.teamworkonline.com/teamwork/r.cfm?i=88100",
-					logo: "angels.gif",
-					date: {
-						posted: {
-							year: 2015,
-							month: 10
-						}
-					},
-					active: true
+		jobs: [
+			{
+				company: "Los Angeles Angels of Anaheim",
+				title: "Director, Quantitative Analysis",
+				location: "Anaheim, CA",
+				description: "Responsible for developing and overseeing the research and analytics initiative within Baseball Operations, reporting to the General Manager and Assistant General Manager.",
+				link: "http://baseballjobs.teamworkonline.com/teamwork/r.cfm?i=88100",
+				logo: "angels.gif",
+				date: {
+					posted: {
+						year: 2015,
+						month: 10
+					}
 				},
-				{
-					company: "TruMedia Networks",
-					title: "Principal / Senior / QA Software",
-					location: "Boston, MA (remote possible)",
-					description: "TruMedia is looking for Principal, Senior and QA Software Engineers to help build tier one sports analytics and scouting solutions. The ideal candidates will be passionate sports fans, have a strong interest in data science and have the ability to work on the full software stack with a focus on web development and user interface design. Interfaces will be needed for web based solutions as well as mobile platforms.",
-					link: "http://www.fangraphs.com/blogs/instagraphs/job-posting-trumedia-networks-principal-senior-qa-software-engineer/",
-					logo: "trumedia.jpg",
-					date: {
-						posted: {
-							year: 2015,
-							month: 10,
-							day: 14
-						}
-					},
-					active: true
+				active: true
+			},
+			{
+				company: "TruMedia Networks",
+				title: "Principal / Senior / QA Software",
+				location: "Boston, MA (remote possible)",
+				description: "TruMedia is looking for Principal, Senior and QA Software Engineers to help build tier one sports analytics and scouting solutions. The ideal candidates will be passionate sports fans, have a strong interest in data science and have the ability to work on the full software stack with a focus on web development and user interface design. Interfaces will be needed for web based solutions as well as mobile platforms.",
+				link: "http://www.fangraphs.com/blogs/instagraphs/job-posting-trumedia-networks-principal-senior-qa-software-engineer/",
+				logo: "trumedia.jpg",
+				date: {
+					posted: {
+						year: 2015,
+						month: 10,
+						day: 14
+					}
 				},
-				{
-					company: "Charlotte Hornets",
-					title: "Business Analytics Internship",
-					location: "Charlotte, NC",
-					description: "The Business Analytics and Database Marketing Intern will be interning in our offices here in Charlotte, NC. The primary responsibility of this intern will be to assist the Business Intelligence department’s needs with business analytics.",
-					link: "http://nbateamjobs.teamworkonline.com/teamwork/r.cfm?i=87460",
-					logo: "hornets.gif",
-					date: {
-						posted: {
-							year: 2015,
-							month: 10,
-							day: 5
-						}
-					},
-					active: true
+				active: true
+			},
+			{
+				company: "Charlotte Hornets",
+				title: "Business Analytics Internship",
+				location: "Charlotte, NC",
+				description: "The Business Analytics and Database Marketing Intern will be interning in our offices here in Charlotte, NC. The primary responsibility of this intern will be to assist the Business Intelligence department’s needs with business analytics.",
+				link: "http://nbateamjobs.teamworkonline.com/teamwork/r.cfm?i=87460",
+				logo: "hornets.gif",
+				date: {
+					posted: {
+						year: 2015,
+						month: 10,
+						day: 5
+					}
 				},
-				{
-					company: "San Francisco 49ers",
-					title: "Data Science Internship",
-					location: "San Francisco, CA",
-					description: "We are looking for a Data Science Intern to play a critical role in our use and understanding of data across various 49ers technology efforts and fan interaction and touch points.",
-					link: "http://49ers.teamworkonline.com/teamwork/r.cfm?i=83653",
-					logo: "49ers.gif",
-					date: {
-						posted: {
-							year: 2015,
-							month: 9,
-						}
-					},
-					active: true
+				active: true
+			},
+			{
+				company: "San Francisco 49ers",
+				title: "Data Science Internship",
+				location: "San Francisco, CA",
+				description: "We are looking for a Data Science Intern to play a critical role in our use and understanding of data across various 49ers technology efforts and fan interaction and touch points.",
+				link: "http://49ers.teamworkonline.com/teamwork/r.cfm?i=83653",
+				logo: "49ers.gif",
+				date: {
+					posted: {
+						year: 2015,
+						month: 9,
+					}
 				},
-				{
-					company: "Columbus Crew SC",
-					title: "Database & Analytics Internship",
-					location: "Columbus, OH",
-					description: "Columbus Crew SC is offering a Database and Analytics Internship designed to offer experience in reviewing and analyzing data within our Sales & Marketing Department.",
-					link: "http://mls.teamworkonline.com/teamwork/r.cfm?i=87042",
-					logo: "columbus-crew.gif",
-					date: {
-						posted: {
-							year: 2015,
-							month: 9,
-							day: 28
-						}
-					},
-					active: true
+				active: true
+			},
+			{
+				company: "Columbus Crew SC",
+				title: "Database & Analytics Internship",
+				location: "Columbus, OH",
+				description: "Columbus Crew SC is offering a Database and Analytics Internship designed to offer experience in reviewing and analyzing data within our Sales & Marketing Department.",
+				link: "http://mls.teamworkonline.com/teamwork/r.cfm?i=87042",
+				logo: "columbus-crew.gif",
+				date: {
+					posted: {
+						year: 2015,
+						month: 9,
+						day: 28
+					}
 				},
-				{
-					company: "Baseball Info Solutions",
-					title: "Research & Development Associate",
-					location: "Allentown, PA",
-					description: "Baseball Info Solutions (BIS) is looking for candidates to fill a full-time position in our R&D Department. The R&D Associate will work out of our office near Allentown, Pa., and will contribute as a member of our R&D team, supporting research for publications and future products.",
-					link: "http://www.fangraphs.com/blogs/instagraphs/job-posting-baseball-info-solutions-research-development-associate/",
-					logo: "bis.gif",
-					date: {
-						posted: {
-							year: 2015,
-							month: 9,
-							day: 28
-						}
-					},
-					active: true
+				active: true
+			},
+			{
+				company: "Baseball Info Solutions",
+				title: "Research & Development Associate",
+				location: "Allentown, PA",
+				description: "Baseball Info Solutions (BIS) is looking for candidates to fill a full-time position in our R&D Department. The R&D Associate will work out of our office near Allentown, Pa., and will contribute as a member of our R&D team, supporting research for publications and future products.",
+				link: "http://www.fangraphs.com/blogs/instagraphs/job-posting-baseball-info-solutions-research-development-associate/",
+				logo: "bis.gif",
+				date: {
+					posted: {
+						year: 2015,
+						month: 9,
+						day: 28
+					}
 				},
-				{
-					company: "Atlanta Braves",
-					title: "Baseball Operations Analyst",
-					location: "Atlanta, GA",
-					description: "Responsibilities: Oversee the in-house technological efforts associated with the development of Braves’ new internal player evaluation application, Development and on-going adjustment of proprietary statistics and systems, etc.",
-					link: "http://www.fangraphs.com/blogs/instagraphs/job-posting-atlanta-braves-baseball-operations-analyst/",
-					logo: "braves.gif",
-					date: {
-						posted: {
-							year: 2015,
-							month: 7,
-							day: 23
-						}
-					},
-					active: true
-				}
-			],
-			closed: [
-				{
-					company: "Carolina Hurricanes",
-					title: "Java Developer/Database Administrator",
-					location: "Raleigh, NC",
-					description: "The Carolina Hurricanes and PNC Arena are seeking a Java Developer to continue development of existing systems and write new applications as needed.",
-					link: "http://hockeyjobs.nhl.com/teamwork/r.cfm?i=85191",
-					logo: "hurricanes.gif",
-					date: {
-						posted: {
-							year: 2015,
-							month: 9,
-						}
-					},
-					active: false
+				active: true
+			},
+			{
+				company: "Atlanta Braves",
+				title: "Baseball Operations Analyst",
+				location: "Atlanta, GA",
+				description: "Responsibilities: Oversee the in-house technological efforts associated with the development of Braves’ new internal player evaluation application, Development and on-going adjustment of proprietary statistics and systems, etc.",
+				link: "http://www.fangraphs.com/blogs/instagraphs/job-posting-atlanta-braves-baseball-operations-analyst/",
+				logo: "braves.gif",
+				date: {
+					posted: {
+						year: 2015,
+						month: 7,
+						day: 23
+					}
 				},
-				{
-					company: "St. Louis Cardinals",
-					title: "Analyst, Baseball Development",
-					location: "St. Louis, MO",
-					description: "This position is responsible for conducting quantitative research to provide timely recommendations to decision makers within Baseball Operations.",
-					link: "http://www.fangraphs.com/blogs/jop-posting-cardinals-openings/",
-					logo: "cardinals.gif",
-					date: {
-						posted: {
-							year: 2015,
-							month: 1,
-							day: 14
-						}
-					},
-					active: false
+				active: true
+			},
+			{
+				company: "Carolina Hurricanes",
+				title: "Java Developer/Database Administrator",
+				location: "Raleigh, NC",
+				description: "The Carolina Hurricanes and PNC Arena are seeking a Java Developer to continue development of existing systems and write new applications as needed.",
+				link: "http://hockeyjobs.nhl.com/teamwork/r.cfm?i=85191",
+				logo: "hurricanes.gif",
+				date: {
+					posted: {
+						year: 2015,
+						month: 9,
+					}
 				},
-				{
-					company: "St. Louis Cardinals",
-					title: "Developer, Baseball Development",
-					location: "St. Louis, MO",
-					description: "This position is responsible for designing, implementing and maintaining analytic and technical systems to provide timely, baseball-related information to decision makers within Baseball Operations.",
-					link: "http://www.fangraphs.com/blogs/jop-posting-cardinals-openings/",
-					logo: "cardinals.gif",
-					date: {
-						posted: {
-							year: 2015,
-							month: 1,
-							day: 14
-						}
-					},
-					active: false
+				active: false
+			},
+			{
+				company: "St. Louis Cardinals",
+				title: "Analyst, Baseball Development",
+				location: "St. Louis, MO",
+				description: "This position is responsible for conducting quantitative research to provide timely recommendations to decision makers within Baseball Operations.",
+				link: "http://www.fangraphs.com/blogs/jop-posting-cardinals-openings/",
+				logo: "cardinals.gif",
+				date: {
+					posted: {
+						year: 2015,
+						month: 1,
+						day: 14
+					}
 				},
-			]
-		}
+				active: false
+			},
+			{
+				company: "St. Louis Cardinals",
+				title: "Developer, Baseball Development",
+				location: "St. Louis, MO",
+				description: "This position is responsible for designing, implementing and maintaining analytic and technical systems to provide timely, baseball-related information to decision makers within Baseball Operations.",
+				link: "http://www.fangraphs.com/blogs/jop-posting-cardinals-openings/",
+				logo: "cardinals.gif",
+				date: {
+					posted: {
+						year: 2015,
+						month: 1,
+						day: 14
+					}
+				},
+				active: false
+			},
+		]
 	};
 
-	Handlebars.registerHelper("job", function(job, closed) {
+	Handlebars.registerHelper("job", function(job, active) {
 		var result = "";
-		result += '<div class="row job-posting">'; // JOB POSTING
+		result += '<div class="row job-posting' + (active ? "" : " job-closed") + '">'; // JOB POSTING
 
 		// add job logo
 		result += '<div class="col-sm-2 col-xs-4">';
@@ -196,9 +192,34 @@
 		result += '<div class="col-sm-7 col-xs-12">';
 		result += '<p>' + job.description + '</p>';
 		result += '<a href="' + job.link + '" target="_new">Link to the ' + job.title + ' job with the ' + job.company + '</a>';
+		if (!active) {
+			result += '<p class="job-closed-text">Please note that this job is now closed.</p>';
+		}
 		result += '</div>';
 
 		result += '</div>'; // / JOB POSTING
+		return new Handlebars.SafeString(result);
+	});
+
+	Handlebars.registerHelper("job-listings", function(jobs) {
+		var result = "";
+
+		activeJobs = '<h2 class="center">Active Jobs</h2>';
+		closedJobs = '<h2 class="center job-closed-text">Closed Jobs</h2>';
+
+		$.each(jobs, function(index, job) {
+			if (job.active) {
+				activeJobs += Handlebars.helpers.job(job, true);
+			} else {
+				closedJobs += Handlebars.helpers.job(job, false);
+			}
+		});
+
+		result += '<div class="row">';
+		result += activeJobs;
+		result += closedJobs;
+		result += '</div>';
+
 		return new Handlebars.SafeString(result);
 	});
 	
@@ -207,6 +228,7 @@
 	//	'{{#each pageData.overview}}',
 	//		'<p>{{{ this }}}</p>',
 	//	'{{/each}}',
+		'{{job-listings pageData.jobs}}',/*
 		'<div class="row">',
 			'<h2 class="center">Active Jobs</h2>',
 			'{{#each pageData.jobs.active}}',
@@ -216,7 +238,7 @@
 			'{{#each pageData.jobs.closed}}',
 				'{{job this}}',
 			'{{/each}}',
-		'</div>'
+		'</div>'*/
 	].join('\n');
 
 	// populate page with the above data
