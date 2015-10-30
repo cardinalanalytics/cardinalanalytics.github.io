d629a83a2e37fb9c2d61eda86185f2ae33ab6455
elishayer <eshayer@stanford.edu>
2015-10-29 17:51:40 -0700

    Meeting date update

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@
 			{
 				image: "calendar",
 				title: "Upcoming Meetings",
-				content: "The next general meeting will be October 18th at 8 pm in School of Education 210. We will also be having sport-specific subgroup meetings in the coming weeks!"
+				content: "The next general meeting will be November 1st at 8 pm in School of Education 210. We will also be having sport-specific subgroup meetings in the coming weeks!"
 			},
 			{
 				image: "comments",
